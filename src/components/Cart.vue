9b674e24dbf0064c4cc2e9a013aadf527298f206
tarazena <tarazena@gmail.com>
2017-12-14 18:00:28 -0500

    added route to go home

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,33 +1,59 @@
 <template>
   <div>
     <div v-if="products.length > 0">
-      <div class="container" style="background-color: white;">
-	<table id="cart" class="table table-hover table-condensed">
-    				<thead>
-						<tr>
-							<th style="width:50%">Product</th>
-							<th style="width:10%">Price</th>
-							<th style="width:8%">Quantity</th>
-							<th style="width:22%" class="text-center">Subtotal</th>
-							<th style="width:10%"></th>
-						</tr>
-					</thead>
-					<tbody>
-						<cart-item v-for="(item, index) in products" :key="index" :cartItem="item" />
-					</tbody>
-					<tfoot>
-						<tr class="visible-xs">
-							<td class="text-center"><strong>Total 1.99</strong></td>
-						</tr>
-						<tr>
-							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i>Continue Shopping</a></td>
-							<td colspan="2" class="hidden-xs"></td>
-							<td class="hidden-xs text-center"><strong>Total ${{total}}</strong></td>
-							<td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
-						</tr>
-					</tfoot>
-				</table>
-</div>
+      <div 
+        class="container" 
+        style="background-color: white;">
+        <table 
+          id="cart" 
+          class="table table-hover table-condensed">
+          <thead>
+            <tr>
+              <th style="width:50%">Product</th>
+              <th style="width:10%">Price</th>
+              <th style="width:8%">Quantity</th>
+              <th 
+                style="width:22%" 
+                class="text-center">Subtotal</th>
+              <th style="width:10%" />
+            </tr>
+          </thead>
+          <tbody>
+            <cart-item 
+              v-for="(item, index) in products" 
+              :key="index" 
+              :cartItem="item" />
+          </tbody>
+          <tfoot>
+            <tr class="visible-xs">
+              <td class="text-center">
+                <strong>Total 1.99</strong>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a 
+                  href="#" 
+                  class="btn btn-warning">
+                <i class="fa fa-angle-left"/><router-link  class="nav-link" :to='"/"'>Continue Shopping</router-link></a>
+              </td>
+              <td 
+                colspan="2" 
+                class="hidden-xs" />
+              <td class="hidden-xs text-center">
+                <strong>Total ${{ total }}</strong>
+              </td>
+              <td>
+                <a 
+                  href="#" 
+                  class="btn btn-success btn-block">Checkout
+                  <i class="fa fa-angle-right" />
+                </a>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
     <div v-else>
       <h1>Your Cart is Empty</h1>
